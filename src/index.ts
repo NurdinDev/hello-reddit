3cd04a7c420b85ba3d194a37c119ed930eee380f
NurdinDev <nureddinbedevi@gmail.com>
2020-09-26 00:35:56 +0300

    Add post schema and get query

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import mikroConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/posts";
 
 const main = async () => {
 	const orm = await MikroORM.init(mikroConfig);
@@ -14,9 +15,10 @@ const main = async () => {
 
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
-			resolvers: [HelloResolver],
+			resolvers: [PostResolver],
 			validate: false,
 		}),
+		context: () => ({em: orm.em})
 	});
 
 	apolloServer.applyMiddleware({ app });
