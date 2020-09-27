9280a6aefa455556f576bcc933c768ebb59ea1a3
NurdinDev <nureddinbedevi@gmail.com>
2020-09-27 23:07:55 +0300

    Rename to tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import {
+  Link as ChakraLink,
+  Text,
+} from '@chakra-ui/core'
+
+import { Container } from '../components/Container'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+
+const Index = () => (
+  <Container>
+      <Text>
+        Example repository of.Î
+      </Text>
+    <DarkModeSwitch />
+  </Container>
+)
+
+export default Index
