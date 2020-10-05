f648a01d206cf4fdfaf0b3fe58fbebcef2b2baa7
NurdinDev <nureddinbedevi@gmail.com>
2020-10-05 23:11:24 +0300

    Refactor app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,80 +1,8 @@
-import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
-import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import { ColorModeProvider, CSSReset, ThemeProvider } from "@chakra-ui/core";
+import React from "react";
 import theme from "../theme";
 
-import React from "react";
-import { cacheExchange, QueryInput, Cache } from "@urql/exchange-graphcache";
-import {
-  LoginMutation,
-  LogoutMutation,
-  MeDocument,
-  MeQuery,
-  RegisterMutation,
-} from "../generated/graphql";
 
-function betterUpdateQuery<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-}
-
-const cache = cacheExchange({
-  updates: {
-    Mutation: {
-      logout: (_result, args, cache, info) => {
-        betterUpdateQuery<LogoutMutation, MeQuery>(
-          cache,
-          { query: MeDocument },
-          _result,
-          () => ({ me: null })
-        );
-      },
-      login: (_result, args, cache, info) => {
-        betterUpdateQuery<LoginMutation, MeQuery>(
-          cache,
-          { query: MeDocument },
-          _result,
-          (result, query) => {
-            if (result.login.errors) {
-              return query;
-            } else {
-              return {
-                me: result.login.user,
-              };
-            }
-          }
-        );
-      },
-      register: (_result, args, cache, info) => {
-        betterUpdateQuery<RegisterMutation, MeQuery>(
-          cache,
-          { query: MeDocument },
-          _result,
-          (result, query) => {
-            if (result.register.errors) {
-              return query;
-            } else {
-              return {
-                me: result.register.user,
-              };
-            }
-          }
-        );
-      },
-    },
-  },
-});
-
-const client = createClient({
-  url: "http://localhost:4000/graphql",
-  fetchOptions: {
-    credentials: "include",
-  },
-  exchanges: [dedupExchange, cache, fetchExchange],
-});
 
 interface appProps {
   Component: any;
@@ -83,14 +11,12 @@ interface appProps {
 
 const MyApp: React.FC<appProps> = ({ Component, pageProps }) => {
   return (
-    <Provider value={client}>
-      <ThemeProvider theme={theme}>
-        <ColorModeProvider>
-          <CSSReset />
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <ColorModeProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ThemeProvider>
   );
 };
 
