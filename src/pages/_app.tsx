5f20cb52b033b906a40731405faea4304c86bc65
NurdinDev <nureddinbedevi@gmail.com>
2020-10-05 07:50:04 +0300

    Fix typescript error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,22 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
 import { Provider, createClient } from "urql";
-const client = createClient({ 
+const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
-    credentials: "include"
-  }
+    credentials: "include",
+  },
 });
 
 import theme from "../theme";
 
-function MyApp({ Component, pageProps }) {
+import React from "react";
+
+interface appProps {
+  Component: any;
+  pageProps: any;
+}
+
+const MyApp: React.FC<appProps> = ({ Component, pageProps }) => {
   return (
     <Provider value={client}>
       <ThemeProvider theme={theme}>
@@ -20,6 +27,6 @@ function MyApp({ Component, pageProps }) {
       </ThemeProvider>
     </Provider>
   );
-}
+};
 
 export default MyApp;
