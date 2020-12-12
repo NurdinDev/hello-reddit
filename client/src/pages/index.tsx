6e49861e7e144a785303a16118641bf30062da22
NurdinDev <nureddinbedevi@gmail.com>
2020-12-12 13:33:06 +0300

    Show posts list on home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,27 +1,54 @@
-import { Link, Text } from "@chakra-ui/core";
-import { withUrqlClient } from "next-urql";
-import { Layout } from "../components/Layout";
-import { createUrqlClient } from "../utils/createUrqlClient";
+import React from 'react';
+import {Box, Button, Flex, Heading, Stack, Text} from "@chakra-ui/core";
+import {withUrqlClient} from "next-urql";
+import {Layout} from "../components/Layout";
+import {createUrqlClient} from "../utils/createUrqlClient";
 import NextLink from "next/link";
-import { usePostsQuery } from "../generated/graphql";
+import {usePostsQuery} from "../generated/graphql";
 
 const Index = () => {
-  const [{ data, fetching }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
-  });
+        const [{data, fetching}] = usePostsQuery({
+            variables: {
+                limit: 10,
+            },
+        });
 
-  return (
-    <Layout>
-      <NextLink href="/create-post">
-        <Link>Create Post</Link>
-      </NextLink>
-      <br />
-      {fetching && <Text>Loading...</Text>}
-      {data && data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
-    </Layout>
-  );
-};
+        if(!fetching && !data) {
+            return (<div>Something wrong! 🙁 check the console log 🐛.</div>)
+        }
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
+        return (
+            <Layout>
+                <Flex justifyContent="space-between" alignItems="center">
+                    <Heading>Hello Reddit 👋</Heading>
+                    <NextLink href="/create-post">
+                        <Button>Create Post</Button>
+                    </NextLink>
+                </Flex>
+                <br/>
+                {!data && fetching ? <Text>Loading...</Text> : (
+                    <Stack spacing={8}> {
+                        data!.posts.map((p) =>
+                            (
+                                <Box key={p.id} p="5" shadow="md" borderWidth="1px">
+                                    <Heading fontSize="xl">{p.title} </Heading>
+                                    <Text mt="4">{p.textSnippet}</Text>
+                                </Box>
+                            )
+                        )
+                    }
+                    </Stack>
+                )}
+                {data && <Flex>
+                    <Button isLoading={fetching} mx={"auto"} my="6" variant="outline"> Load More 👻 </Button>
+                </Flex>}
+            </Layout>
+        );
+    }
+;
+
+export default withUrqlClient(createUrqlClient,
+    {
+        ssr: true
+    }
+)(Index);
