ed5393bbd4fb525d6fc9193776cef61b02ae4639
NurdinDev <nureddinbedevi@gmail.com>
2020-12-12 22:02:24 +0300

    Load posts on index page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import {Box, Button, Flex, Heading, Stack, Text} from "@chakra-ui/core";
+import {Button, Flex, Heading} from "@chakra-ui/core";
 import {withUrqlClient} from "next-urql";
 import {Layout} from "../components/Layout";
 import {createUrqlClient} from "../utils/createUrqlClient";
 import NextLink from "next/link";
-import {usePostsQuery} from "../generated/graphql";
+import Posts from "../components/Posts";
 
 const Index = () => {
-        const [{data, fetching}] = usePostsQuery({
-            variables: {
-                limit: 10,
-            },
-        });
 
-        if(!fetching && !data) {
-            return (<div>Something wrong! 🙁 check the console log 🐛.</div>)
-        }
 
         return (
             <Layout>
@@ -26,22 +18,8 @@ const Index = () => {
                     </NextLink>
                 </Flex>
                 <br/>
-                {!data && fetching ? <Text>Loading...</Text> : (
-                    <Stack spacing={8}> {
-                        data!.posts.map((p) =>
-                            (
-                                <Box key={p.id} p="5" shadow="md" borderWidth="1px">
-                                    <Heading fontSize="xl">{p.title} </Heading>
-                                    <Text mt="4">{p.textSnippet}</Text>
-                                </Box>
-                            )
-                        )
-                    }
-                    </Stack>
-                )}
-                {data && <Flex>
-                    <Button isLoading={fetching} mx={"auto"} my="6" variant="outline"> Load More 👻 </Button>
-                </Flex>}
+
+                <Posts/>
             </Layout>
         );
     }
