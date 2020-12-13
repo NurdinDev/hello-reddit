bb53c01ec3f909a2dc80bb2fe01dc74a4e17c8fc
NurdinDev <nureddinbedevi@gmail.com>
2020-12-13 16:49:41 +0300

    Add check before vote for current user and previously voted user

--- server/src/resolvers/posts.ts
+++ server/src/resolvers/posts.ts
@@ -16,6 +16,7 @@ import {
 import { MyContext } from '../types';
 import { isAuth } from '../middleware/isAuth';
 import { getConnection } from 'typeorm';
+import { Upvote } from '../entity/Upvote';
 
 @InputType()
 class PostInput {
@@ -123,26 +124,60 @@ export class PostResolver {
         const realValue = value > -1 ? 1 : -1;
 
         // prevent the current user voting on his posts
-        // @TODO
+        const post = await Post.findOne(postId);
+        if (post?.creatorId === userId) {
+            // you can vote your post
+            return false;
+        }
 
-        await getConnection().transaction(async (tm) => {
-            await tm.query(
-                `
-                    insert into upvote ("userId", "postId", "value")
-                    values ($1, $2, $3)
-                `,
-                [userId, postId, realValue],
-            );
+        const vote = await Upvote.findOne({ where: { postId, userId } });
 
-            await tm.query(
-                `
-                update post
-                set points = points + $1
-                where id = $2
-            `,
-                [realValue, postId],
-            );
-        });
+        if (vote && vote.value !== realValue) {
+            // same user and post but he changed his mind
+            await getConnection().transaction(async (tm) => {
+                await tm.query(
+                    `
+                        update upvote
+                        set value = $1
+                        where "postId" = $2
+                          and "userId" = $3
+                    `,
+                    [realValue, postId, userId],
+                );
+                await tm.query(
+                    `
+                        update post
+                        set points = points + $1
+                        where id = $2
+                    `,
+                    [realValue, postId],
+                );
+            });
+        } else if (!vote) {
+            // new vote
+            await getConnection().transaction(async (tm) => {
+                await tm.query(
+                    `
+                        insert into upvote ("userId", "postId", "value")
+                        values ($1, $2, $3)
+                    `,
+                    [userId, postId, realValue],
+                );
+
+                await tm.query(
+                    `
+                        update post
+                        set points = points + $1
+                        where id = $2
+                    `,
+                    [realValue, postId],
+                );
+            });
+        } else {
+            // previously voted
+            return false;
+        }
+
         return true;
     }
 }
