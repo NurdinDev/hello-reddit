06180c7c1775a534b93265c30cc101c1e1c97a85
NurdinDev <nureddinbedevi@gmail.com>
2020-12-13 16:28:10 +0300

    Add upvote entity and relation with posts and users

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,41 +1,45 @@
-import { Field, ID, ObjectType } from "type-graphql";
+import { Field, ID, ObjectType } from 'type-graphql';
 import {
-  BaseEntity,
-  Column,
-  CreateDateColumn,
-  Entity,
-  OneToMany,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from "typeorm";
-import { Post } from "./Post";
+    BaseEntity,
+    Column,
+    CreateDateColumn,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { Post } from './Post';
+import { Upvote } from './Upvote';
 
 @ObjectType()
 @Entity()
 export class User extends BaseEntity {
-  @Field(() => ID)
-  @PrimaryGeneratedColumn()
-  id!: number;
+    @Field(() => ID)
+    @PrimaryGeneratedColumn()
+    id!: number;
 
-  @Field()
-  @Column({ unique: true })
-  email!: string;
+    @Field()
+    @Column({ unique: true })
+    email!: string;
 
-  @Field()
-  @Column({ unique: true })
-  username!: string;
+    @Field()
+    @Column({ unique: true })
+    username!: string;
 
-  @Column()
-  password!: string;
+    @Column()
+    password!: string;
 
-  @OneToMany(() => Post, (post) => post.creator)
-  posts: Post[];
+    @OneToMany(() => Post, (post) => post.creator)
+    posts: Post[];
 
-  @Field(() => String)
-  @CreateDateColumn()
-  createdAt: Date;
+    @OneToMany(() => Upvote, (upvote) => upvote.user)
+    upvotes: Upvote[];
 
-  @Field(() => String)
-  @UpdateDateColumn()
-  updatedAt: Date;
+    @Field(() => String)
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Field(() => String)
+    @UpdateDateColumn()
+    updatedAt: Date;
 }
