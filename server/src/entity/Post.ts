d7b0bd9510463ebbf8c9112ae1be73b0429bb6c6
NurdinDev <nureddinbedevi@gmail.com>
2020-12-13 07:46:22 +0300

    Add creator to graphql field

--- server/src/entity/Post.ts
+++ server/src/entity/Post.ts
@@ -1,46 +1,47 @@
-import { Field, ObjectType } from "type-graphql";
+import { Field, ObjectType } from 'type-graphql';
 import {
-  BaseEntity,
-  Column,
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from "typeorm";
-import { User } from "./User";
+    BaseEntity,
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { User } from './User';
 
 @ObjectType()
 @Entity()
 export class Post extends BaseEntity {
-  @Field()
-  @PrimaryGeneratedColumn()
-  id!: number;
+    @Field()
+    @PrimaryGeneratedColumn()
+    id!: number;
 
-  @Field()
-  @Column()
-  title!: string;
+    @Field()
+    @Column()
+    title!: string;
 
-  @Field()
-  @Column()
-  text!: string;
+    @Field()
+    @Column()
+    text!: string;
 
-  @Field()
-  @Column({ type: "int", default: 0 })
-  points!: number;
+    @Field()
+    @Column({ type: 'int', default: 0 })
+    points!: number;
 
-  @Field()
-  @Column()
-  creatorId: number;
+    @Field()
+    @Column()
+    creatorId: number;
 
-  @ManyToOne(() => User, (user) => user.posts)
-  creator: User;
+    @Field()
+    @ManyToOne(() => User, (user) => user.posts)
+    creator: User;
 
-  @Field(() => String)
-  @CreateDateColumn()
-  createdAt: Date;
+    @Field(() => String)
+    @CreateDateColumn()
+    createdAt: Date;
 
-  @Field(() => String)
-  @UpdateDateColumn()
-  updatedAt: Date;
+    @Field(() => String)
+    @UpdateDateColumn()
+    updatedAt: Date;
 }
