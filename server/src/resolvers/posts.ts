f64efd6466ec4d0042eefcbfaf7b4e7ea123d230
NurdinDev <nureddinbedevi@gmail.com>
2020-12-14 00:18:22 +0300

    fix: add graphql types correctly to vote mutation

--- server/src/resolvers/posts.ts
+++ server/src/resolvers/posts.ts
@@ -116,8 +116,8 @@ export class PostResolver {
     @Mutation(() => Boolean)
     @UseMiddleware(isAuth)
     async vote(
-        @Arg('postId') postId: number,
-        @Arg('value') value: number,
+        @Arg('postId', () => Int) postId: number,
+        @Arg('value', () => Int) value: number,
         @Ctx() { req }: MyContext,
     ): Promise<boolean> {
         const { userId } = req.session;
