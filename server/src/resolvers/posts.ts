780bba34c34060e431720b310967319a1f03df2a
NurdinDev <nureddinbedevi@gmail.com>
2020-12-16 21:33:57 +0300

    feat: add post update resolver

--- server/src/resolvers/posts.ts
+++ server/src/resolvers/posts.ts
@@ -59,6 +59,9 @@ export class PostResolver {
         if (cursor) {
             replacements.push(new Date(parseInt(cursor)));
         }
+
+        console.log(replacements);
+
         const posts = await getConnection().query(
             `
         select p.*,
@@ -101,20 +104,21 @@ export class PostResolver {
     }
 
     @Mutation(() => Post, { nullable: true })
+    @UseMiddleware(isAuth)
     async updatePost(
-        @Arg('id') id: number,
-        @Arg('title', () => String, { nullable: true }) title: string,
+        @Arg('id', () => Int) id: number,
+        @Arg('title') title: string,
+        @Arg('text') text: string,
+        @Ctx() { req }: MyContext,
     ): Promise<Post | null> {
-        const post = await Post.findOne(id);
-        if (!post) {
-            return null;
-        }
-
-        if (typeof title !== undefined) {
-            await Post.update({ id }, { title });
-        }
-
-        return post;
+        const result = await getConnection()
+            .createQueryBuilder()
+            .update(Post)
+            .set({ title, text })
+            .where('id =:id and "creatorId" = :creatorId', { id, creatorId: req.session.userId })
+            .returning('*')
+            .execute();
+        return result.raw[0];
     }
 
     @Mutation(() => Boolean)
