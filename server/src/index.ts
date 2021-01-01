b97c63f2e73f23b0a61cd857b88afe6a4e7171e0
NurdinDev <nureddinbedevi@gmail.com>
2021-01-01 15:49:35 +0300

    feat: add retries logic to database connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,35 +1,67 @@
+import { Post } from './entity/Post';
+import { User } from './entity/User';
+import { Upvote } from './entity/Upvote';
 import { ApolloServer } from 'apollo-server-express';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
-import 'dotenv-safe/config';
 import express from 'express';
 import session from 'express-session';
 import Redis from 'ioredis';
 import 'reflect-metadata';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
-import { COOKI_NAME, __prod__ } from './constants';
+import { __prod__, COOKI_NAME } from './constants';
 import { PostResolver } from './resolvers/posts';
 import { UserResolver } from './resolvers/users';
 import { createUserLoader } from './utils/createUserLoader';
 import { createUpVoteLoader } from './utils/createUpVoteLoader';
+import path from 'path';
 
 const main = async () => {
-    await createConnection();
+    let retries = 5;
 
-    // await conn.runMigrations();
-
+    while (retries) {
+        try {
+            const conn = __prod__
+                ? await createConnection({
+                      type: 'postgres',
+                      url: process.env.DATABASE_URL,
+                      logging: true,
+                      migrations: [path.join(__dirname, './migration/*')],
+                      entities: [Post, User, Upvote],
+                  })
+                : await createConnection();
+            if (__prod__) {
+                console.log('Running migration 🚀');
+                if (process.env.NODE_ENV === 'production') {
+                    await conn.runMigrations();
+                }
+                console.log('Migration Done! 🚀');
+            }
+            break;
+        } catch (err) {
+            console.log(err);
+            retries -= 1;
+            console.log(`retries left: ${retries}`);
+            // wait 5 seconds
+            await new Promise((res) => setTimeout(res, 5000));
+        }
+    }
     const app = express();
 
     let RedisStore = connectRedis(session);
-    let redis = new Redis();
-
+    let redis = new Redis(process.env.REDIS_URL);
     app.use(
         cors({
             origin: process.env.CORS_ORIGIN,
             credentials: true,
         }),
     );
+
+    if (__prod__) {
+        app.set('trust proxy', 1); // trust first proxy
+    }
+
     app.use(
         session({
             name: COOKI_NAME,
@@ -42,6 +74,7 @@ const main = async () => {
                 httpOnly: true,
                 sameSite: 'lax', // csrf
                 secure: __prod__, // cookies only works on https
+                domain: __prod__ ? '.nurdin.dev' : undefined,
             },
             saveUninitialized: false,
             secret: process.env.SESSION_SECRET as string,
@@ -66,7 +99,7 @@ const main = async () => {
     apolloServer.applyMiddleware({ app, cors: false });
 
     app.listen(process.env.PORT, () => {
-        console.log('server started on localhost:4000');
+        console.log(`server started on ${process.env.PORT}`);
     });
 };
 
